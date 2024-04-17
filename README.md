27880396a434e1cc7b70e263c300de8962f9499e
bhagabata30 <bhagabata@kloudkradle.com>
2024-04-17 10:15:53 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# React + Vite + Tailwind Css Template
+
+
+# first Clone the repository the run below commands
+
+# npm install
+
+# npm run dev
+
+# for productoion run
+
+
---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.css';
+
+import './charts/ChartjsConfig';
+
+// Import pages
+import Dashboard from './pages/Dashboard';
+import Customers from  './pages/Customers';
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/ecommerce/customers" element={<Customers />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
